1c0e9daa806931e1221d015dd9ab5e4634f8a4e0
dflasher <rzswh@163.com>
2020-04-25 16:32:12 +0800

    sys_create_exception_channel wont fail on Thread/Process

--- zircon-object/src/task/thread.rs
+++ zircon-object/src/task/thread.rs
@@ -1,4 +1,5 @@
 use {
+    super::exception::*,
     super::process::Process,
     super::*,
     crate::object::*,
@@ -89,6 +90,7 @@ pub struct Thread {
     proc: Arc<Process>,
     ext: Box<dyn Any + Send + Sync>,
     inner: Mutex<ThreadInner>,
+    exceptionate: Arc<Exceptionate>,
 }
 
 impl_kobject!(Thread
@@ -140,6 +142,7 @@ impl Thread {
             _counter: CountHelper::new(),
             proc: proc.clone(),
             ext: Box::new(ext),
+            exceptionate: Exceptionate::new(ZxExceptionChannelType::Thread),
             inner: Mutex::new(ThreadInner {
                 context: Some(Box::new(UserContext::default())),
                 ..Default::default()
@@ -338,6 +341,10 @@ impl Thread {
         } else {
             ThreadState::Suspended
         }
+    }
+
+    pub fn get_exceptionate(&self) -> Arc<Exceptionate> {
+        self.exceptionate.clone()
     }
 }
 
